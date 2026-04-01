856a31b8e25abc8fccc9eb432f61daba9808174a
Pascal Moser <pascalmoser@Pascals-MacBook-Pro-2.local>
2026-04-01 19:33:37 +0200

    wip

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Logo from "./Logo";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <Counter />
+    </>
+  );
+}
+
+export default App;
